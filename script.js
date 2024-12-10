8741856c8d27e1012e31dd71c7f297209c2b806a
Vivesh kumar <vivesh103@gmail.com>
2024-12-10 21:09:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	// writer code here...
+	if (year % 4 ==0 && year %100 !=0 || year %400==0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+let year = prompt("Enter the Year: ");
+result = daysOfAYear(year);
+if (year == true){
+	return 366;
+}
+else{
+	return 365;
+}
